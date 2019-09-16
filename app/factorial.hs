6770b5c17a58b4b23086af359c940ce75c2a062f
Craig Fay <craigfay1@gmail.com>
2019-09-15 23:25:05 -0500

    Non-recursive factorials

--- app/factorial.hs
+++ app/factorial.hs
@@ -1,4 +1,9 @@
 factorial :: Int -> Int
 
+-- Factorials with Recursion
 factorial 0 = 1
 factorial n = n * factorial (n - 1)
+
+-- Factorials without Recursion
+productFactorial n = product [1..n]
+
