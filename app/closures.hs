55297ff51d6c2909e904adc334621cbd89f0caa8
Craig Fay <craigfay1@gmail.com>
2019-09-17 00:03:14 -0500

    Returning functions

--- app/closures.hs
+++ app/closures.hs
@@ -10,5 +10,13 @@ doMult :: (Int -> Int) -> Int
 
 -- Given a function, invoke it using 3 as input
 doMult fn = fn 3
-
 product3And4 = doMult times4
+
+-- Instead of receiving a function, return a function
+getAddFunc :: Int -> (Int -> Int)
+
+getAddFn x y = x + y
+
+adds3 = getAddFn 3
+
+sum3And4 = adds3 4
