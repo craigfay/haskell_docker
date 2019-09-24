0ab1b37f6f1fb3b988cf1eed54f7c48d9fd6766c
Craig Fay <craigfay1@gmail.com>
2019-09-23 22:06:11 -0400

    Heron formula

---
+++ app/heron.hs
@@ -0,0 +1,9 @@
+-- Heron's Formula is used to calculate the area of a triangle given the length
+-- of sides a, b, and c.
+
+-- heron :: Int -> Int -> Int -> Int
+
+heron a b c = sqrt (s*(s-a)*(s-b)*(s-c))
+  where
+  s = (a+b+c) / 2
+
